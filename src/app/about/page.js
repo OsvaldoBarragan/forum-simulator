17f0b73d1b459399e7b87d6c29c12056c85637b9
Osvaldo <barragan_osvaldo39@gmail.com>
2024-05-12 03:28:21 -0400

    Create a header for each route; add new route

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-export default function Forums() {
+export default function About() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <header className="flex flex-row justify-between items-center p-8 min-h-32 min-w-64 w-2/4 bg-gradient-to-b from-blue-800">
@@ -8,7 +8,7 @@ export default function Forums() {
                 <Link className="text-blue-500" href="/about">About</Link>
                 <Link className="text-blue-500" href="/forums">Forums</Link>
             </header>
-            <h1>Hello, Forums</h1>
+            <h1>This is the about page that will explain what the purpose of this site is.</h1>
         </main>
     );
 }
